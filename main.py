52f485b681a2b85bf6a983676cf87c36c5020e4b
shash <shashanka5398@gmail.com>
2024-06-02 16:29:35 +0530

    returning not found for robots.txt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import xtracto
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 uvicorn_kwargs = {
     "host": "0.0.0.0",
@@ -7,9 +8,16 @@ uvicorn_kwargs = {
 
 }
 app = FastAPI()
+
+
+@app.get("/robots.txt")
+async def NotFound():
+    return JSONResponse({"detail": "not found"}, 404)
+
+
 xtracto.App(app, uvicorn_kwargs=uvicorn_kwargs, auto_run=False)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, **uvicorn_kwargs)
 
+    uvicorn.run(app, **uvicorn_kwargs)
